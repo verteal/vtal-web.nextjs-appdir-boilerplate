18164fde1a939df775bc2c93cc898d4fea4c7ed2
fgouvea10 <felipegalvesouvea@gmail.com>
2024-02-26 21:25:24 -0300

    feat: remove pages folder + add app dir

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react'
+
+type RootLayoutProps = {
+  children: ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
